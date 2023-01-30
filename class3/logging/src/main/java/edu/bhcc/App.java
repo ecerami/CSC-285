1fb7f94ca8a0f03fe5647f182771e4d6bba05353
Ethan Cerami <cerami@gmail.com>
2023-01-30 11:49:53 -0500

    Added

---
+++ class3/logging/src/main/java/edu/bhcc/App.java
@@ -0,0 +1,20 @@
+package edu.bhcc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Logger logger = LoggerFactory.getLogger(App.class);
+        logger.info("This is an info message");
+        logger.warn("This is a warning");
+        logger.error("This is an error");
+        System.out.println( "Hello World!" );
+    }
+}
