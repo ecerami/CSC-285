685bdf0c265a396eb59bd23eeab032c8e667a1d4
Ethan Cerami <cerami@gmail.com>
2023-01-30 13:34:32 -0500

    Added

---
+++ class3/junit/src/main/java/edu/bhcc/SimpleMath.java
@@ -0,0 +1,22 @@
+package edu.bhcc;
+
+/**
+ * Rectangle Class.
+ *
+ */
+public class SimpleMath {
+
+    /**
+     * Add Two Integers.
+     */
+    static public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Multiple Two Integers.
+     */
+    static public int multiply(int a, int b) {
+        return a * b;
+    }
+}
---
+++ class3/junit/src/test/java/edu/bhcc/SimpleMathTest.java
@@ -0,0 +1,22 @@
+package edu.bhcc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class SimpleMathTest {
+    @Test
+    public void testAddition() {
+        int answer = SimpleMath.add(2, 3);
+        assertEquals(5, answer);
+    }
+
+    @Test
+    public void testMultiplication() {
+        int answer = SimpleMath.multiply(2, 3);
+        assertEquals(6, answer);
+    }
+}
