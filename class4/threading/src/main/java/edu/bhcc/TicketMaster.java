19f4e49f0ab18a39194d99dabab66f204dd13544
ecerami <cerami@gmail.com>
2023-02-05 17:05:41 -0500

    Added TicketMaster

---
+++ class4/threading/src/main/java/edu/bhcc/TicketMaster.java
@@ -0,0 +1,31 @@
+package edu.bhcc;
+
+/**
+ * Simulate TicketMaster Selling Lots of Tickets.
+ */
+public class TicketMaster {
+    private int numTicketsToSell;
+
+    /**
+     * Constructor with total number of tickets available to sell.
+     * @param numTicketsToSell
+     */
+    public TicketMaster(int numTicketsToSell) {
+        this.numTicketsToSell = numTicketsToSell;
+    }
+
+    /**
+     * Buy a Single Ticket.  Not thread-safe.
+     */
+    public void buyTicket() {
+        int updatedNumTickets = numTicketsToSell - 1;
+
+        // Introduce a tiny delay to magnify the problem.
+        try {
+            Thread.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        this.numTicketsToSell = updatedNumTickets;
+    }
+}
