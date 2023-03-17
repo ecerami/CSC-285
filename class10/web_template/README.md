eb327842e67328fee2228b39d359b053f3cd75f9
ecerami <cerami@gmail.com>
2023-03-17 09:42:18 -0400

    Created web template

---
+++ class10/web_template/README.md
@@ -0,0 +1,8 @@
+# Jetty Web Application Template
+
+This directory contains a template for creating and running servlets
+via the embedded Jetty engine.
+
+To run the server:
+
+mvn jetty:run
---
+++ class10/web_template/src/main/java/edu/bhcc/HelloServlet.java
@@ -0,0 +1,19 @@
+package edu.bhcc;
+
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet
+{
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+    {
+        response.setContentType("text/html");
+        response.setStatus(HttpServletResponse.SC_OK);
+
+        response.getWriter().println("<h1>Hello, World!</h1>");
+        response.getWriter().println("session=" + request.getSession(true).getId());
+    }
+}
