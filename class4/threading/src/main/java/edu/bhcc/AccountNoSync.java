6e0e1f145a77611bcd3debd0e2bf15af4b5bf455
ecerami <cerami@gmail.com>
2024-02-22 10:35:40 -0500

    Updated again.

--- class4/threading/src/main/java/edu/bhcc/AccountNoSync.java
+++ class4/threading/src/main/java/edu/bhcc/AccountNoSync.java
@@ -18,7 +18,7 @@ public class AccountNoSync {
      * Deposit the specified amount.
      * @param amount amount to add.
      */
-    public synchronized void deposit (int amount) {
+    public void deposit (int amount) {
         int newBalance = this.balance + amount;
 
         // Introduce a tiny delay to magnify the problem.
